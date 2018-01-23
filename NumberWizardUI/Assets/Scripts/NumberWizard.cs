53731e0a9bcbb41d03bfc330511d17bdffc090b6
José Saúco Delgado <jose.saucod@gmail.com>
2018-01-23 13:46:47 +0100

    Initial Upload for NumberWizardUI

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	public Text guessText;
+	int max = 1000;
+	int min = 1;
+	int guess;
+	
+	// Use this for initialization
+	void Start () {
+		NextGuess();
+		max++;
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();	
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();	
+	}
+	
+	void NextGuess(){
+		//guess = (max+min)/2;
+		guess = Random.Range (min,max);
+		Debug.Log ("Guess: " + guess);
+		guessText.text = guess.ToString();
+	}
+}
