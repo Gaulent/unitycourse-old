33c42798e400cd36f4b897a18c79aec1a4959574
José Saúco Delgado <jose.saucod@gmail.com>
2018-01-26 13:32:27 +0100

    Initial upload of Block Breaker

---
+++ BlockBreaker/Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	
+	void OnTriggerEnter2D (Collider2D c2d) {
+		Debug.Log ("OnTriggerEnter2D --> " + c2d.ToString());
+	}
+	
+	void OnCollisionEnter2D (Collision2D c2d) {
+		Debug.Log ("OnCollisionEnter2D --> " + c2d.ToString());
+		levelManager.LoadLevel("Win Screen");
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Awake() will be called before Start() as soon as the objects are initialised
+	void Awake () {
+		if (instance != null) {
+			Destroy(gameObject);
+			Debug.Log ("Duplicate music player self-destructing!");
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		GameObject.DontDestroyOnLoad(gameObject);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}	
+}
