e8b8462cd682a2dce86cfb9eaaf6ad1e09c5d3ff
José Saúco Delgado <jose.saucod@gmail.com>
2018-02-11 13:16:09 +0100

    Creation of Laser Defender

---
+++ LaserDefender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("New Level load: " + name);
+		Brick.breakableCount = 0;
+		Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel() {
+		Debug.Log ("New Level load: " + name);
+		Brick.breakableCount = 0;
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void BrickDestroyed() {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Requested to quit game");
+		Application.Quit ();
+	}
+	
+}
---
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Awake() will be called before Start() as soon as the objects are initialised
+	// more info on: https://docs.unity3d.com/Manual/ExecutionOrder.html
+	// Game Patterns: http://gameprogrammingpatterns.com/
+	
+	void Awake () {
+		Debug.Log(this.GetInstanceID());
+		if (instance != null) {
+			Destroy(gameObject);
+			Debug.Log ("Duplicate music player self-destructing!");
+			Debug.Log(this.GetInstanceID());
+		} else {
+			instance = this;
+			this.audio.Play();
+			GameObject.DontDestroyOnLoad(gameObject);	
+		}
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}	
+}
