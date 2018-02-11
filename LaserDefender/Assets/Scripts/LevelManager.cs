cd0f0471112c6cc9cbf759e894e98cb8b43f8956
José Saúco Delgado <jose.saucod@gmail.com>
2018-02-11 14:20:59 +0100

    Some cleaning on Laser Defender

--- LaserDefender/Assets/Scripts/LevelManager.cs
+++ LaserDefender/Assets/Scripts/LevelManager.cs
@@ -5,20 +5,12 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("New Level load: " + name);
-		Brick.breakableCount = 0;
 		Application.LoadLevel(name);
 	}
 
 	public void LoadNextLevel() {
-		Debug.Log ("New Level load: " + name);
-		Brick.breakableCount = 0;
+		Debug.Log ("Loading Next Level.");
 		Application.LoadLevel(Application.loadedLevel + 1);
-	}
-	
-	public void BrickDestroyed() {
-		if (Brick.breakableCount <= 0) {
-			LoadNextLevel();
-		}
 	}
 	
 	public void QuitRequest() {
--- LaserDefender/Assets/Scripts/MusicPlayer.cs
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -10,20 +10,12 @@ public class MusicPlayer : MonoBehaviour {
 	// Game Patterns: http://gameprogrammingpatterns.com/
 	
 	void Awake () {
-		Debug.Log(this.GetInstanceID());
 		if (instance != null) {
 			Destroy(gameObject);
-			Debug.Log ("Duplicate music player self-destructing!");
-			Debug.Log(this.GetInstanceID());
 		} else {
 			instance = this;
 			this.audio.Play();
 			GameObject.DontDestroyOnLoad(gameObject);	
 		}
 	}
-		
-	// Update is called once per frame
-	void Update () {
-		
-	}	
 }
