b966c3c7e67c816df4ff069ae856580c5f71aef1
José Saúco Delgado <jose.saucod@gmail.com>
2018-02-12 14:40:16 +0100

    Lase Defender, enemies moving

---
+++ LaserDefender/Assets/Entities/Enemyformation/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 1f;
+	public float padding = 1f;
+
+	private float xmin,xmax;
+	private int direction = -1;
+		
+	// Use this for initialization
+	void Start () {
+		// La salida de Instantiate develve un Object, lo parseamos a GameObject
+		
+		foreach(Transform child in transform) {
+			GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
+
+			// Pone el objeto creado bajo este en la jerarquia
+			enemy.transform.parent = child;
+		}
+		
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+	
+	public void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += Vector3.right * speed * Time.deltaTime * direction;
+		
+		if (transform.position.x <= xmin+padding)
+			direction = 1;
+		if (transform.position.x >= xmax-padding)
+			direction = -1;
+		
+	}
+}
---
+++ LaserDefender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 8f;
+	public float padding = 1f;
+
+	private float xmin,xmax;
+	
+
+
+	void Start () {
+	
+		// ViewportToWorldPoint devuelve puntos en el espacio en referencia a la camara
+		// En este caso la camara Camera.main
+	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+	
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+		
+	}
+	
+	
+	void Update () {
+	
+		// Time.deltaTime es el tiempo entre frames. Haciendolo independiente del framerate.
+	
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+		
+	}
+}
