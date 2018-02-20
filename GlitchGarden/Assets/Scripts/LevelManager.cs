d1b4ae83fa1f8617070378f4c06d9121711f3ce4
José Saúco Delgado <jose.saucod@gmail.com>
2018-02-20 08:28:49 +0100

    Glitch Garden. Initial Import

---
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel() {
+		Debug.Log ("Loading Next Level.");
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Requested to quit game");
+		Application.Quit ();
+	}
+	
+}
---
+++ GlitchGarden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+	
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	private AudioSource music;
+	
+	// Awake() will be called before Start() as soon as the objects are initialised
+	// more info on: https://docs.unity3d.com/Manual/ExecutionOrder.html
+	// Game Patterns: http://gameprogrammingpatterns.com/
+	
+	void Start() {
+		if (instance != null && instance != this) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);	
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
+		}
+	}
+	
+	
+	// Nuevo "metodo automatico" que se llama con el ID del audio que ha sido llamado.
+	void OnLevelWasLoaded(int level) {
+		Debug.Log ("MusicPlayer: loaded level " + level);
+		if(music) {
+			music.Stop();
+	
+			if(level==0)
+				music.clip = startClip;
+			else if(level==1)
+				music.clip = gameClip;
+			else if(level==2)
+				music.clip = endClip;
+			music.loop = true;
+			music.Play();
+		}
+	}
+}
