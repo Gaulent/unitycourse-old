b8db8ea7c9af6d1ff776a12c5510a730cbad7231
José Saúco Delgado <jose.saucod@gmail.com>
2018-03-07 14:12:24 +0100

    Hacer el FadeIn por animacion en lugar de por script, prefab de los personajes, y restructuracion de los animators de los attackers incluyendo el uso de Eventos en la animacion.

---
+++ GlitchGarden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)] // OHMYGAWD
+	public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		// Añade en script un componente. En este caso el rigidbody, ya que es muy simple.
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D () {
+		Debug.Log (name + " trigger enter");
+	
+	}
+	
+	void setSpeed (float Speed) {
+		currentSpeed = Speed;
+	}
+	
+	void StrikeCurrentTarget(float damage) {
+		Debug.Log(name + " damage dealt.");
+	}
+}
