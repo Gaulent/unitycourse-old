e1456608e42cea8f40fb9c923f48099835204066
José Saúco Delgado <jose.saucod@gmail.com>
2018-03-21 14:22:32 +0100

    Fixing Defenders Animations

---
+++ GlitchGarden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed = 1f;
+	public float damage = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+}
