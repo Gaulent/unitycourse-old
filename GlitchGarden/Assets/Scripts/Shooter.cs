61621aa842d7e2882d2810dfb02297fbbeb7fac4
José Saúco Delgado <jose.saucod@gmail.com>
2018-03-21 20:57:48 +0100

    Keep animating the projectiles

---
+++ GlitchGarden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile,projectileParent;
+
+	private void FireGun() {
+		GameObject newProjectile = Instantiate (projectile, transform.FindChild("Gun").position, Quaternion.identity) as GameObject;
+		newProjectile.transform.parent = projectileParent.transform;
+	}
+}
