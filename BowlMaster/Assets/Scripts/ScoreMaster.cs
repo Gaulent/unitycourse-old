53d14821db5af4f2ac322f4f1fa2e40f2b1414ff
José Saúco Delgado <jose.saucod@gmail.com>
2018-03-24 17:58:37 +0100

    BownMaster. New Project

---
+++ BowlMaster/Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static double ScoreFrames(List<int> n) {
+        return 0;
+    }
+}
