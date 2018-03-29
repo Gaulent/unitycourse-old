f9660707b813fa7d06517239da15bcd44fb8c109
José Saúco Delgado <jose.saucod@gmail.com>
2018-03-29 18:23:36 +0200

    BowlingMaster. Animating the swipper.

--- BowlMaster/Assets/Scripts/Ball.cs
+++ BowlMaster/Assets/Scripts/Ball.cs
@@ -8,19 +8,31 @@ public class Ball : MonoBehaviour {
 	
 	private Rigidbody rigidBody;
 	private AudioSource ballSound;
+	private bool gameStarted = false;
 	
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		ballSound = GetComponent<AudioSource>();
-		LaunchBall();
+		rigidBody.useGravity = false;
+		//Launch(launchSpeed);
 	}
 
-	public void LaunchBall ()
-	{
-		rigidBody.velocity = launchSpeed;
+	public void Launch (Vector3 velocity) {
+		rigidBody.velocity = velocity;
+		rigidBody.useGravity = true;
 		ballSound.Play ();
+		gameStarted = true;
 	}
+
+	public void MoveStart(float xNudge) {
+		if(!gameStarted) {
+			Vector3 newposition = transform.position + Vector3.right * xNudge;
+			newposition.x = Mathf.Clamp(newposition.x, -50, 50);
+			transform.position = newposition;
+		}
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
---
+++ BowlMaster/Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+
+	private Ball ball;
+	private float startTime;
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Ball>();
+	}
+
+	public void DragStart() {
+		startTime = Time.time;
+		startPosition = Input.mousePosition;
+	}
+
+	public void DragEnd() {
+		Vector3 velocity = (Input.mousePosition - startPosition) / (Time.time - startTime);
+		velocity = new Vector3(velocity.x, 0, velocity.y);
+		ball.Launch(velocity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
