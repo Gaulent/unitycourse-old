6ba1464a91d930031081df2263f66d43e610978f
José Saúco Delgado <jose.saucod@gmail.com>
2018-04-02 00:35:07 +0200

    Bowlmaster. Detecting stand up pins and reverse shredder

---
+++ BowlMaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	[Range(0f,1f)]
+	public float standingThreshold = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print(IsStanding());
+	}
+
+	public bool IsStanding() {
+		// transform.rotation.eulerAngles; convierte la rotacion de quaternion a angulos
+		Vector2 position = new Vector2 (transform.rotation.x, transform.rotation.z);
+		return (position.magnitude<=standingThreshold);
+	}
+}
---
+++ BowlMaster/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text standingDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		standingDisplay.text = CountStanding().ToString();
+	}
+
+	int CountStanding() {
+		Pin[] pins = FindObjectsOfType<Pin>();
+		int cont = 0;
+
+		foreach (Pin pin in pins) {
+			if (pin.IsStanding())
+				cont++;
+		}
+
+		return cont;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		GameObject go = col.gameObject;
+		if (go.GetComponent<Ball>()) {
+			print("Ball Entered");
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		GameObject go = col.gameObject.transform.parent.gameObject;
+		print("Something");
+		if (go.GetComponentInParent<Pin>()) {
+			Destroy(go);
+		}
+	}
+}
