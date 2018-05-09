a049fd4c9802d97657fd78b8ea84778f5559c60b
José Saúco Delgado <jose.saucod@gmail.com>
2018-05-09 13:38:16 +0200

    Zombie Runner. Arboles, cesped, agua, zombie, navmesh, spawnpoints, comienzo de helicoptero, zoom... de todo. Importar: Paquete standard: Effects

---
+++ ZombieRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private GameObject[] spawnPoints;
+	public bool spawn = false;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spawn) {
+			ReSpawn();
+			spawn = false;
+		}
+	}
+	
+	void ReSpawn() {
+		GameObject placeToSpawn = spawnPoints[Random.Range(0,spawnPoints.Length)];
+		transform.position = placeToSpawn.transform.position;
+	}
+}
+
+
