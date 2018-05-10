5a3718132a54914f030a1a8bba4d5bd37790da30
José Saúco Delgado <jose.saucod@gmail.com>
2018-05-10 14:37:08 +0200

    Importacion de assests. Me quedé en "configure avatar"

--- ZombieRunner/Assets/Scripts/Helicopter.cs
+++ ZombieRunner/Assets/Scripts/Helicopter.cs
@@ -6,17 +6,20 @@ public class Helicopter : MonoBehaviour {
 
 	public AudioClip callSound;
 	
+	private AudioSource	audioSource;
 	private bool called = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(!called && Input.GetButtonDown("CallHeli")) {
 			called = true;
+			audioSource.clip = callSound;
+			audioSource.Play();
 			Debug.Log("Helicopter called");
 		}
 	}
