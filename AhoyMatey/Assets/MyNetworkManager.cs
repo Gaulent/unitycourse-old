b7d29088c9f1c282457441d54da5502be95299a4
José Saúco Delgado <jose.saucod@gmail.com>
2018-05-20 19:47:31 +0200

    AhoyMatey. Prueba de red

---
+++ AhoyMatey/Assets/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	private NetworkClient netCli;
+
+	public void MyStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + ": Starting Host");
+		netCli = StartHost();
+	}
+
+	public override void OnStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + ": Host Started");
+	}
+
+	public override void OnStartClient(NetworkClient aClient) {
+		Debug.Log(Time.timeSinceLevelLoad + ": Client Started");
+	}
+
+	public override void OnClientConnect(NetworkConnection aConn) {
+		Debug.Log(Time.timeSinceLevelLoad + ": Client connected at " + aConn.address);
+	}
+}
