a8f20c4d8383b9defcd75956161d313092872476
José Saúco Delgado <jose.saucod@gmail.com>
2018-05-22 14:41:19 +0200

    Import de CrossInput. Local Player Authority. El movimiento en red funciona!

---
+++ AhoyMatey/Assets/Players/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+using UnityEngine;
+
+// NetworkBehavious añade cosas chulas de red a MonoBehaviour
+public class Player : NetworkBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer) {
+			return;
+		}
+		
+		inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+		
+		transform.Translate(inputValue);
+	}
+}
