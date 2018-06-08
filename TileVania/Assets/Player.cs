ba047c1312a3687988ece486d5183a2f1f4194b7
José Saúco Delgado <jose.saucod@gmail.com>
2018-06-08 14:23:50 +0200

    Animating TileVania

---
+++ TileVania/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate(speed * Vector3.right * Time.deltaTime * CrossPlatformInputManager.GetAxis("Horizontal"));
+	}
+}
