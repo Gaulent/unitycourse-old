47954c0e40f4ff5aa6b7aff3d8367f68f58e05e0
José Saúco Delgado <jose.saucod@gmail.com>
2018-06-14 14:41:57 +0200

    Climbing ladders

--- TileVania/Assets/Player.cs
+++ TileVania/Assets/Player.cs
@@ -7,33 +7,55 @@ public class Player : MonoBehaviour {
 
 	// Lo pone en el editor aunque sea privado
 	[SerializeField] public float speed = 10f;
+	[SerializeField] public float climbSpeed = 10f;
 	[SerializeField] public float jumpPotency = 5f;
 	private Rigidbody2D rb;
 	private SpriteRenderer sr;
 	private Animator anim;
+	private Collider2D col;
+	private float gravityScale;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
+		col = GetComponent<Collider2D>();
+		gravityScale = rb.gravityScale;
+		
 	}
 	
 	void Update () {
 
 		Run();
 		Jump();
+		Climb();
 	}
 
 	void Jump () {
 
-		if(CrossPlatformInputManager.GetButtonDown("Jump")) {
-			Debug.Log("Log");
+		if(CrossPlatformInputManager.GetButtonDown("Jump") && col.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
 			rb.velocity+=(new Vector2(0,jumpPotency));
 		}
 	}
 
+	void Climb () {
+		float verAxis = CrossPlatformInputManager.GetAxis("Vertical");
+		if(Mathf.Abs(verAxis)>Mathf.Epsilon && col.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {
+			rb.velocity = new Vector2(rb.velocity.x, climbSpeed /** Time.deltaTime*/ * verAxis);
+			anim.SetBool("climbing",true);
+			rb.gravityScale = 0 ;
+			
+		}
 
+			
+		if(!col.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {
+			anim.SetBool("climbing",false);
+			rb.gravityScale = gravityScale;
+		}
+		
+	}
+	
 	void Run () {
 		//Mathf.Epsilon es casi cero.
 		float horAxis = CrossPlatformInputManager.GetAxis("Horizontal");
